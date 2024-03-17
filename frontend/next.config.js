6e59a1603c8bb27966293a3c199178033f5ad4a0
Konda Sandeep <139792713+SandeepOCT@users.noreply.github.com>
2024-03-17 22:10:25 +0530

    add crossOrigin to anonymous

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,27 +6,7 @@ const nextConfig = {
   basePath: config.base_path !== "/" ? config.base_path : "",
   trailingSlash: config.site.trailing_slash,
   output: 'standalone',
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'X-Frame-Options', value: 'SameOrigin' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'X-XSS-Protection', value: '0' },
-          { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
-          {
-            key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              script-src 'self' https://*.vercel.app;
-              img-src 'self'
-            `,
-          },
-        ],
-      },
-    ];
-  }
+  crossOrigin: "anonymous"
 };
 
 module.exports = nextConfig;
