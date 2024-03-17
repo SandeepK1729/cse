7f903ba49894d3e8f5cc2a68dab901be32106ffd
Konda Sandeep <139792713+SandeepOCT@users.noreply.github.com>
2024-03-17 22:05:02 +0530

    modify headers options in next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,6 +6,27 @@ const nextConfig = {
   basePath: config.base_path !== "/" ? config.base_path : "",
   trailingSlash: config.site.trailing_slash,
   output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'X-Frame-Options', value: 'SameOrigin' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-XSS-Protection', value: '0' },
+          { key: 'Referrer-Policy', value: 'origin-when-cross-origin' },
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' https://*.vercel.app;
+              img-src 'self'
+            `,
+          },
+        ],
+      },
+    ];
+  }
 };
 
 module.exports = nextConfig;
